768fca3a4b02790e0d57ffc84f6f88b9c87e77ad
rongmei.li <rongmei.li@unisoc.com>
2019-11-22 16:44:55 +0800

    Failed to show hold call when user click swap and merge simultaneously
    Set EVENT_MERGE_START for foregroundConnection and backgroundConnection
    but don't set EVENT_MERGE_COMPLETE when there is ImsException
    
    Bug: 121105030
    Change-Id: I08c189b976227ada1298a864f6f4ba70c4ffb7f9

--- src/java/com/android/internal/telephony/imsphone/ImsPhoneCallTracker.java
+++ src/java/com/android/internal/telephony/imsphone/ImsPhoneCallTracker.java
@@ -1489,6 +1489,7 @@ public class ImsPhoneCallTracker extends CallTracker implements ImsPullCall {
             fgImsCall.merge(bgImsCall);
         } catch (ImsException e) {
             log("conference " + e.getMessage());
+            handleConferenceFailed(foregroundConnection, backgroundConnection);
         }
     }
 
@@ -4330,5 +4331,16 @@ public class ImsPhoneCallTracker extends CallTracker implements ImsPullCall {
     @Override
     public ImsPhone getPhone() {
         return mPhone;
+    }
+
+    private void handleConferenceFailed(ImsPhoneConnection fgConnection,
+            ImsPhoneConnection bgConnection) {
+        if (fgConnection != null) {
+            fgConnection.handleMergeComplete();
+        }
+        if (bgConnection != null) {
+            bgConnection.handleMergeComplete();
+        }
+        mPhone.notifySuppServiceFailed(Phone.SuppService.CONFERENCE);
     }
 }
